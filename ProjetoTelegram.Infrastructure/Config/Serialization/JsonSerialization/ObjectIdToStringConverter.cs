ca6f2f6ee4f0af0a888a03c7f66eb9bc637a4e76
Lucas Mariano dos Santos <lucasmsantos0153@gmail.com>
2024-11-30 11:59:45 -0300

    refactor: movendo configurações de Json para Infrasctructure

--- ProjetoTelegram.Infrastructure/Config/Serialization/JsonSerialization/ObjectIdToStringConverter.cs
+++ ProjetoTelegram.Infrastructure/Config/Serialization/JsonSerialization/ObjectIdToStringConverter.cs
@@ -2,7 +2,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace ProjetoTelegram.Domain.Config.JsonConverters
+namespace ProjetoTelegram.Infrastructure.Config.Serialization.JsonSerialization
 {
     public class ObjectIdToStringConverter : JsonConverter<ObjectId>
     {
